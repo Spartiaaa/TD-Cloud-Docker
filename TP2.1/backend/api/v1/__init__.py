c1bb8286a4afb4287b81754a543c70392ea0462c
Spartia <Enzo.savoglou@outlook.fr>
2025-12-24 15:59:07 +0100

    Delete TP2.1 directory

--- TP2.1/backend/api/v1/__init__.py
+++
@@ -1,4 +0,0 @@
-from .hotel_routeur import routeur
-from .chambre_routeur import routeur
-from .client_routeur import routeur
-from .reservation_routeur import routeur
--- TP2.1/backend/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .hotel_model import Hotel
-from .chambre_model import Chambre
-from .client_model import Client
-from .reservation_model import Reservation
